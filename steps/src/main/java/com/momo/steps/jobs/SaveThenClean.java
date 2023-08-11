381c13ffa0e057bdb0bcbf19f616a0560b6741bf
Nam Van Do <nam.dv@coccoc.com>
2023-08-11 17:54:03 +0700

    add comments

--- steps/src/main/java/com/momo/steps/jobs/SaveThenClean.java
+++ steps/src/main/java/com/momo/steps/jobs/SaveThenClean.java
@@ -1,5 +1,11 @@
 package com.momo.steps.jobs;
 
 public interface SaveThenClean {
+	/**
+	 * The cache values are updated in the database once
+	 * the newer entries are available, and after updating steps,
+	 * the old entries doesn't belong to current time (day, week, month)
+	 * will be evicted from the cache.
+	 */
 	void saveThenClean();
 }
