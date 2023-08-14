ea87a6432f0f65159443cf3bf15b383312d90d60
Nam Van Do <nam.dv@coccoc.com>
2023-08-14 18:03:40 +0700

    Add comments

--- leaderboard/src/main/java/com/momo/leaderboard/scorer/LeaderboardService.java
+++ leaderboard/src/main/java/com/momo/leaderboard/scorer/LeaderboardService.java
@@ -4,7 +4,19 @@ import com.momo.leaderboard.response.Leaderboard;
 import com.momo.leaderboard.response.StepItem;
 
 public interface LeaderboardService {
+	/**
+	 * Returns the top users have walked the most in a given time period.
+	 *
+	 * @param top number of top items to return
+	 * @return the top users have walked the most in a given time
+	 */
 	Leaderboard getLeaderBoard(int top);
+
+	/**
+	 * Update the leaderboard if necessary based on the given step item.
+	 *
+	 * @param stepItem a new step item has been recorded
+	 */
 	void processLeaderboardItem(StepItem stepItem);
 }
 
